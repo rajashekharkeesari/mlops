755522f370e6d718b178e2968fc3c400c806ff86
keesari Rajashekhar <rajashekharkeesari@gmail.com>
2025-10-23 14:41:21 +0530

    file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-set -e
+set -e  # Exit immediately if a command fails
 
-echo "Pulling DVC artifacts from S3..."
-dvc pull --remote myremote --force --quiet
+echo "Checking DVC artifacts..."
+if [ ! -d ".dvc/cache" ]; then
+    echo "Pulling DVC artifacts from S3..."
+    dvc pull --remote myremote --quiet
+else
+    echo "DVC artifacts already present. Skipping pull."
+fi
 
 echo "Starting Flask API..."
-python src/app.py
+exec python src/app.py  # Use exec to replace the shell process with Python
