6cc833aa7abeda10b364da021ec1593bc6a675b5
keesari Rajashekhar <rajashekharkeesari@gmail.com>
2025-10-25 12:28:25 +0530

    Fix GitHub Actions workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-set -e  # Exit immediately if a command fails
+set -e
 
 echo "Checking DVC artifacts..."
-if [ ! -d ".dvc/cache" ]; then
+if [ ! -d "/app/.dvc/cache" ]; then
     echo "Pulling DVC artifacts from S3..."
     dvc pull --remote myremote --quiet
-else
-    echo "DVC artifacts already present. Skipping pull."
 fi
 
+echo "Starting MLflow server..."
+mlflow server --host 0.0.0.0 --port 5000 \
+  --backend-store-uri /app/mlruns \
+  --default-artifact-root /app/mlruns &
+
 echo "Starting Flask API..."
-exec python src/app.py  # Use exec to replace the shell process with Python
+exec python /app/src/app.py
